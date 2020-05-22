12ac303473c7187b897ab0818a21a76612f166aa
Romaric Pascal <hello@romaricpascal.is>
2020-05-22 22:28:04 +0100

    feat: use suffix at end of file to detect language

--- src/plugins/detectLanguage.js
+++ src/plugins/detectLanguage.js
@@ -1,4 +1,6 @@
-module.exports = function detectLanguage({ languageInfo = pathPrefix } = {}) {
+module.exports = function detectLanguage({
+  languageInfo = getLanguageInfo
+} = {}) {
   return function(files, metalsmith) {
     const { i18n } = metalsmith.metadata();
 
@@ -7,6 +9,21 @@ module.exports = function detectLanguage({ languageInfo = pathPrefix } = {}) {
     });
   };
 };
+
+function getLanguageInfo(file, options) {
+  return (
+    filenameSuffix(file, options) ||
+    pathPrefix(file, options) ||
+    defaultLanguageInfo(file, options)
+  );
+}
+
+function defaultLanguageInfo(file, { defaultLanguage = 'en' } = {}) {
+  return {
+    key: file.pathInfo.stem,
+    language: defaultLanguage
+  };
+}
 
 function pathPrefix(
   file,
@@ -19,10 +36,15 @@ function pathPrefix(
       language: result[1],
       key: result[2]
     };
-  } else {
+  }
+}
+
+function filenameSuffix(file, { separator = '--' } = {}) {
+  const [key, language] = file.pathInfo.stem.split(separator);
+  if (language) {
     return {
-      language: defaultLanguage,
-      key: file.pathInfo.stem
+      key,
+      language: language
     };
   }
 }
---
+++ thoughts-and-drafts/site-rebuild/detecting-language-differently.md
@@ -0,0 +1,10 @@
+Detecting language differently
+===
+
+Use a BEM-like modifier to set the language:
+
+- co-location of translated articles
+- leave extensions for "file type", keeping things separated
+
+Separate responsibilities of providing default
+Create filenameSuffix function
