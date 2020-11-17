011aac10194611d06d918c7a425aafd64b48d3c3
Romaric Pascal <hello@romaricpascal.is>
2020-11-17 09:01:50 +0000

    feat: start reinstating site layout

---
+++ nuxt/components/LanguageSwitcher.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="language-switcher" aria-labelledby="languageSwitcherHeading">
+    <h2 id="languageSwitcherHeading" hidden>Language</h2>
+    <a href="/" aria-current="page">English</a>
+    <a href="/fr">Français</a>
+  </nav>
+</template>
