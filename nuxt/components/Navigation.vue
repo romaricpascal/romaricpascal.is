cc798c53e6b41b0e586540d0d507ac0e0866a088
Romaric Pascal <hello@romaricpascal.is>
2020-11-25 17:49:24 +0000

    feat: add internationalization of posts link

--- nuxt/components/Navigation.vue
+++ nuxt/components/Navigation.vue
@@ -1,3 +1,14 @@
+<i18n>
+{
+  "en": {
+    "posts": "All posts"
+  },
+  "fr": {
+    "posts": "Tous les posts"
+  }
+}
+</i18n>
+
 <template>
   <nav aria-labelledby="site-navigation-label">
     <h2 id="site-navigation-label" hidden>Site</h2>
@@ -8,7 +19,7 @@
     >
       Romaric Pascal
     </nuxt-link>
-    <nuxt-link :to="localePath('/posts')">All posts</nuxt-link>
+    <nuxt-link :to="localePath('/posts')">{{ $t('posts') }}</nuxt-link>
     <nuxt-link :to="localePath(`/${me.route}`)">{{ me.title }}</nuxt-link>
     <a href="mailto:hello@romaricpascal.is">Contact</a>
   </nav>
