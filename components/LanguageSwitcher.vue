9edbdfe435477e87be8c73eff4fce0840658c44c
Romaric Pascal <hello@romaricpascal.is>
2020-12-09 16:50:03 +0000

    refactor: base language switcher on available locales

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,7 +1,12 @@
 <template>
   <nav class="language-switcher" aria-labelledby="languageSwitcherHeading">
     <h2 id="languageSwitcherHeading" hidden>Language</h2>
-    <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
-    <nuxt-link :to="switchLocalePath('fr')">Français</nuxt-link>
+    <nuxt-link
+      v-for="(locale, index) in $i18n.locales"
+      :key="index"
+      :exact="true"
+      :to="switchLocalePath(locale.code)"
+      >{{ locale.name }}</nuxt-link
+    >
   </nav>
 </template>
