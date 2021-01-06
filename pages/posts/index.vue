34c9991b58e7b84a36fba67be107af287823f843
Romaric Pascal <hello@romaricpascal.is>
2021-01-06 11:49:05 +0000

    refactor: extract pagination logic in its own module

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -35,6 +35,7 @@
 
 <script>
 import head from '~/mixins/head'
+import { paginate } from '~/lib/paginate'
 
 export default {
   mixins: [head],
@@ -76,31 +77,4 @@ export default {
     prose: false,
   },
 }
-
-const PER_PAGE = 5
-
-function paginate(data, { page = 1, perPage = paginate.PER_PAGE } = {}) {
-  const lastPage = Math.ceil(data.length / perPage)
-
-  const isInRange = page >= 0 && page <= lastPage
-  if (!isInRange) return null
-
-  const startIndex = (page - 1) * perPage
-  const endIndex = Math.min(startIndex + perPage, data.length)
-
-  const previousPage = page > 1 ? page - 1 : null
-  // Quick cast of page as an integer
-  const nextPage = page < lastPage ? page * 1 + 1 : null
-
-  return {
-    firstPage: 1,
-    startIndex,
-    endIndex,
-    lastPage,
-    previousPage,
-    nextPage,
-  }
-}
-
-paginate.PER_PAGE = PER_PAGE
 </script>
